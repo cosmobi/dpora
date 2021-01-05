e2c4ff2faf01253137b180b8f825cba8e03a6ff1
cosmobi <fischler@live.com>
2021-01-05 17:43:58 +0100

    Added icon buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 // wrap DporaApp widget within a MaterialApp widget
 void main() => runApp(MaterialApp(home: DporaApp()));
@@ -10,8 +11,7 @@ class DporaApp extends StatefulWidget {
 
 class _DporaAppState extends State<DporaApp> {
   // updatable content from DB
-  String stimText =
-      'Mind on your money, or money on your mind? And would you rather sip on gin or juice, or both?';
+  String stimText = 'Mind on your money. Money on your mind. Sippin on gin and juice. West Side, yall!';
   Color userColor = Colors.greenAccent; // userText updated in userOutput below
   String tileTextLT = 'left top text here';
   Color textColorLT = Colors.orangeAccent;
@@ -20,7 +20,7 @@ class _DporaAppState extends State<DporaApp> {
   String tileTextRT =
       'this text is about 25 words or something maybe longer. this text is about 25 words or maybe shorter. this text is about 25 words or so.';
   Color textColorRT = Colors.purpleAccent;
-  String tileTextRB = 'right bottom text here';
+  String tileTextRB = 'right bottom text here and not there';
   Color textColorRB = Colors.redAccent;
 
   @override
@@ -62,24 +62,79 @@ class _DporaAppState extends State<DporaApp> {
 
   Widget _stimulus(textSize) {
     return Container(
-      padding: EdgeInsets.all(10),
-      margin: EdgeInsets.all(10),
-      child: Text(
-        stimText,
-        style: TextStyle(
-            fontSize: textSize,
-            color: Colors.yellow), // yellowAccent is too bright
-      ),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.yellow),
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-        color: Colors.black,
-      ),
-    );
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.yellow),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          color: Colors.grey[900],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SizedBox(
+                    height: 20,
+                    width: 20,
+                    child: IconButton(
+                      icon: Icon(Icons.menu_rounded, color: Colors.grey[700],),
+                      padding: EdgeInsets.zero, // need for alignment
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: 20,
+                    child: IconButton(
+                      icon: Icon(Icons.share_rounded, color: Colors.grey[700],),
+                      padding: EdgeInsets.zero, // need for alignment
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: 20,
+                    child: IconButton(
+                      icon: Icon(Icons.threesixty_rounded, color: Colors.grey[700],),
+                      padding: EdgeInsets.zero, // need for alignment
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: 20,
+                    child: IconButton(
+                      icon: Icon(Icons.cancel_presentation_rounded, color: Colors.grey[700],),
+                      padding: EdgeInsets.zero, // need for alignment
+                      onPressed: () {},
+                    ),
+                  ),
+                ]
+            ),
+            Flexible(
+              child: 
+              SingleChildScrollView(
+                padding: EdgeInsets.fromLTRB(15.0, 5.0, 10.0, 5.0),
+                child: Text(
+                  stimText,
+                  style: TextStyle(
+                      fontSize: textSize,
+                      color: Colors.yellow, //yellowAccent is too bright
+                      ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 
   Widget _userInput() {
     return TextField(
+      style: TextStyle(color: userColor),
+      cursorColor: userColor,
       decoration: InputDecoration(
         enabledBorder: OutlineInputBorder(
           borderSide: BorderSide(color: userColor),
@@ -90,11 +145,16 @@ class _DporaAppState extends State<DporaApp> {
         border: OutlineInputBorder(
           borderSide: BorderSide(color: userColor),
         ),
+        fillColor: Colors.grey[900], filled: true,
+        labelText: 'Responses disappear after 30 seconds. Be brief. Be respectful.',
         labelStyle: TextStyle(color: userColor),
-        labelText: 'You\'re anonymous & responses are erased after 30 seconds.',
         hintText: 'Type here and hit enter on the keyboard.',
+        hintStyle: TextStyle(color: Colors.grey[700]),
       ),
-      autofocus: true,
+      maxLength: 255, // this is way more than needed but allows for ascii art
+      //
+      autofocus: true, // SET TO FALSE IF IT DOESNT GO AWAY AFTER A SUBMISSION
+    //
     );
   }
 
@@ -105,10 +165,15 @@ class _DporaAppState extends State<DporaApp> {
       decoration: BoxDecoration(
         border: Border.all(color: userColor),
         borderRadius: BorderRadius.all(Radius.circular(20)),
-        color: Colors.black,
+        color: Colors.grey[900],
       ),
-      child: Text('what the user wrote goes here',
-          style: TextStyle(fontSize: textSize, color: userColor)),
+      child: 
+        Text(
+          'what the user wrote goes directly here',
+          style: TextStyle(fontSize: textSize, color: userColor),
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis, // truncates, not scrollable
+        ),
     );
   }
 
@@ -121,16 +186,59 @@ class _DporaAppState extends State<DporaApp> {
       decoration: BoxDecoration(
         border: Border.all(color: textColor),
         borderRadius: BorderRadius.all(Radius.circular(20)),
-        color: Colors.black,
+        color: Colors.grey[900],
       ),
-      child: SingleChildScrollView(
-        child: Text(
-          tileText,
-          style: TextStyle(fontSize: textSize, color: textColor),
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            child: SingleChildScrollView(
+              child: Text(
+                tileText,
+                style: TextStyle(fontSize: textSize, color: textColor),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Flexible( 
+                child: Text('From USA',
+                  style: TextStyle(
+                    fontSize: 12, 
+                    fontStyle: FontStyle.italic,
+                    color: Colors.grey[700],
+                  ),
+                  overflow: TextOverflow.ellipsis, // truncates, not scrollable
+                ),
+              ),
+            Row(children: [
+              //
+              Text('0', // NEED TO PUT A COUNTER HERE
+                //
+                  style: TextStyle(
+                    fontSize: 12, 
+                    color: Colors.grey[700],
+                  ),
+                ),
+              SizedBox(
+                height: 20,
+                width: 20,
+                child: IconButton(
+                  icon: Icon(Icons.person_remove_outlined, color: Colors.grey[700],),
+                  padding: EdgeInsets.zero, // need for alignment
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ]
       ),
-    );
-  }
+    ]
+  ),
+);
+}
 
   Widget _buildVerticalLayout(
     tileTextLT,
@@ -146,18 +254,13 @@ class _DporaAppState extends State<DporaApp> {
     final allTileWidth = 0.46; // 46% screen width
     final allTextSize = 18; // 18 font size
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Expanded(
-          child: Container(),
-        ), // spacer
         Container(
           height: MediaQuery.of(context).size.height * 0.23, //23%
           width: MediaQuery.of(context).size.width, //100%
           child: _stimulus(24),
         ),
-        Expanded(
-          child: Container(),
-        ), // spacer
         // build the chat titles, the left column (top & bottom) and right
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -201,24 +304,15 @@ class _DporaAppState extends State<DporaApp> {
             ),
           ],
         ),
-        Expanded(
-          child: Container(),
-        ), // spacer
         Container(
           width: MediaQuery.of(context).size.width,
           child: _userOutput(18), // font size 18
         ),
-        Expanded(
-          child: Container(),
-        ), // spacer
         Container(
           padding: EdgeInsets.all(10),
           width: MediaQuery.of(context).size.width,
           child: _userInput(),
         ),
-        Expanded(
-          child: Container(),
-        ), // spacer
       ],
     );
   }
@@ -236,10 +330,9 @@ class _DporaAppState extends State<DporaApp> {
     final allTileHeight = 0.33; // 33% screen height
     final allTileWidth = 0.21; // 21% screen width
     final allTextSize = 26; // 26 font size
-    return Column(children: [
-      Expanded(
-        child: Container(),
-      ), // spacer
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -306,17 +399,11 @@ class _DporaAppState extends State<DporaApp> {
           )
         ],
       ),
-      Expanded(
-        child: Container(),
-      ), // spacer
       Container(
         width: MediaQuery.of(context).size.width * 0.95, //95%
         padding: EdgeInsets.all(10),
         child: _userInput(),
       ),
-      Expanded(
-        child: Container(),
-      ), // spacer
     ]);
   }
-}
+  }
--- notes/comments.dart
+++ notes/comments.dart
@@ -1,169 +1,39 @@
-//
-// put in these lines to view the visual layout at runtime.
-// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
-// (...whcih goes in the import section on the very top)
-// debugPaintSizeEnabled = true; 
-// (...which goes right after void main()
-//
-    // Text(
-    //  'Kandersteg, Switzerland',
-    //   style: TextStyle(
-    //   color: Colors.grey[500],
-    //  ),
-    //
-    //  Icon(
-    //    Icons.star,
-    //    color: Colors.red[500],
-    //  ),
-//
-    // Color color = Theme.of(context).primaryColor;
-//
-    // Widget buttonSection = Container(
-    //   child: Column(
-    //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    //     children: [
-    //       _buildButtonColumn(color, Icons.call, 'CALL'),
-    //       _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-    //       _buildButtonColumn(color, Icons.share, 'SHARE'),
-    //     ],
-    //   ),
-    // );
-//
-    // Widget chatGridSection() => Container(
-    //       decoration: BoxDecoration(
-    //         color: Colors.black26,
-    //       ),
-    //       child: Column(
-    //         children: [
-    //           Text("hello"),
-    //           Text("goodbye"),
-    //         ],
-    //       ),
-    //     );
-//
-    // Widget chatGridSection2() => Container(
-    // child:
-    // GridView.count(
-    //       primary: false,
-    //       padding: const EdgeInsets.all(4),
-    //       crossAxisSpacing: 4,
-    //       mainAxisSpacing: 4,
-    //       crossAxisCount: 3,
-    //       children: <Widget>[
-    //         Container(
-    //           padding: const EdgeInsets.all(20),
-    //           child: Center(
-    //             child: const Text('FIRST'),
-    //           ),
-    //           color: Colors.red,
-    //         ),
-    //         Container(
-    //           padding: const EdgeInsets.all(20),
-    //           child: Center(
-    //             child: const Text('SECOND'),
-    //           ),
-    //           color: Colors.indigo,
-    //         ),
-    //         Container(
-    //           padding: const EdgeInsets.all(20),
-    //           child: Center(
-    //             child: const Text('THIRD'),
-    //           ),
-    //           color: Colors.teal,
-    //         ),
-    //         Container(
-    //           padding: const EdgeInsets.all(20),
-    //           child: Center(
-    //             child: const Text('FOURTH'),
-    //           ),
-    //           color: Colors.cyan,
-    //         ),
-    //       ],
-    //     ),);
-        //
-        //      appBar: AppBar(
-        //        title: Text('dpora'),
-        //      ),
-        // body:
-        // GridView.count(
-        //   primary: false,
-        //   padding: const EdgeInsets.all(4),
-        //   crossAxisSpacing: 4,
-        //   mainAxisSpacing: 4,
-        //   crossAxisCount: 3,
-        //   children: <Widget>[
-        //     Container(
-        //       padding: const EdgeInsets.all(20),
-        //       child: Center(
-        //         child: const Text('FIRST'),
-        //       ),
-        //       color: Colors.red,
-        //     ),
-        //     Container(
-        //       padding: const EdgeInsets.all(20),
-        //       child: Center(
-        //         child: const Text('SECOND'),
-        //       ),
-        //       color: Colors.indigo,
-        //     ),
-        //     Container(
-        //       padding: const EdgeInsets.all(20),
-        //       child: Center(
-        //         child: const Text('THIRD'),
-        //       ),
-        //       color: Colors.teal,
-        //     ),
-        //     Container(
-        //       padding: const EdgeInsets.all(20),
-        //       child: Center(
-        //         child: const Text('FOURTH'),
-        //       ),
-        //       color: Colors.cyan,
-        //     ),
-        //   ],
-        // ),
-//
-// Column(
-//   children: <Widget>[
-//     Text('simbox will go here'),
-//     Expanded(
-//       child: FittedBox(
-//         fit: BoxFit.contain, // otherwise the logo will be tiny
-//       //  child: const FlutterLogo(),
-//       child: chatGridSection(),
-//       ),
-//     ),
-//     // Container(
-//     //   child: chatGridSection2(),
-//     // ),
-//     Text('user input here'),
-//   ],
-// )
 
-//Image.asset(
-//  'images/favicon_bg.png',
-//  width: 600,
-//  height: 240,
-//  fit: BoxFit.cover,
-//),
+//     Widget buttonSection = Container(
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: [
+//           _buildButtonColumn(color, Icons.call, 'CALL'),
+//           _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+//           _buildButtonColumn(color, Icons.share, 'SHARE'),
+//         ],
+//       ),
+//     );
 
 // Column _buildButtonColumn(Color color, IconData icon, String label) {
-//   return Column(
-//     mainAxisSize: MainAxisSize.min,
-//     mainAxisAlignment: MainAxisAlignment.center,
-//     children: [
-//       Icon(icon, color: color),
-//       Container(
-//         margin: const EdgeInsets.only(top: 8),
-//         child: Text(
-//           label,
-//           style: TextStyle(
-//             fontSize: 12,
-//             fontWeight: FontWeight.w400,
-//             color: color,
-//           ),
-//         ),
-//       ),
-//     ],
-//   );
-// }
+//   return 
+  
+  // Column(
+  //   mainAxisSize: MainAxisSize.min,
+  //   mainAxisAlignment: MainAxisAlignment.center,
+  //   children: [
+  //     Icon(Icons.menu, color: Colors.grey),
+  //     Icon(Icons.share, color: Colors.grey),
+  //     Icon(Icons.next_plan, color: Colors.grey),
+  //     Icon(Icons.indeterminate_check_box_rounded, color: Colors.grey),
+  //   ]
+  // )
+
+    //   Container(
+    //     margin: const EdgeInsets.only(top: 8),
+    //     child: Text(
+    //       label,
+    //       style: TextStyle(
+    //         fontSize: 12,
+    //         fontWeight: FontWeight.w400,
+    //         color: color,
+    //       ),
+    //     ),
+    //   ),
+    // ],
+ 
